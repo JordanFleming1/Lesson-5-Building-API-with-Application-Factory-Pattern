5fbd088665d4c37933671af2dd44fc5c6a30a18d
Jordan Fleming <insanitylegend35@gmail.com>
2025-11-28 06:55:22 -0500

    Upload workflow GitHub actions

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, send_from_directory, jsonify, render_template_string
 from app.extensions import db, ma, cache, limiter
 from app.config import DevelopmentConfig
 from app.blueprints import register_blueprints
+import os
 
 
 def create_app():
@@ -14,4 +15,38 @@ def create_app():
     # Optional: Set a global default rate limit
     app.config['RATELIMIT_DEFAULT'] = '100 per hour'
     register_blueprints(app)
+
+    # Serve swagger.yaml
+    @app.route('/swagger.yaml')
+    def swagger_yaml():
+        return send_from_directory(
+            os.path.join(app.root_path, 'static'),
+            'swagger.yaml',
+            mimetype='text/yaml'
+        )
+
+    # Serve Swagger UI at /docs
+    @app.route('/docs')
+    def swagger_ui():
+        return render_template_string('''
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Swagger UI</title>
+          <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+        </head>
+        <body>
+        <div id="swagger-ui"></div>
+        <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+        <script>
+          const ui = SwaggerUIBundle({
+            url: '/swagger.yaml',
+            dom_id: '#swagger-ui',
+          });
+        </script>
+        </body>
+        </html>
+        ''')
+
     return app
+
